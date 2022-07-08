c52ab8664b0e0da294ad85e4b769b1d2cb75d680
Drothy <dorothychrisman@gmail.com>
2022-07-07 19:50:54 -0700

    Initial Commit

---
+++ recipe finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipe finder
+//
+//  Created by Dorothy Chrisman on 7/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recipe finder/recipe_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_finderApp.swift
+//  recipe finder
+//
+//  Created by Dorothy Chrisman on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct recipe_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
